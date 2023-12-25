5c8913cd54330d2ee3f1a2c853f080c23a8cb935
Stefan Caloian <stefc12341@outlook.com>
2023-12-24 19:14:31 -0500

    added terminal stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Terminal from './components/Terminal';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="terminal-container">
+        <Terminal />
+      </div>
+    </div>
+  );
+}
+
+export default App;
